50958ac5200f4274dc3b756c9f7935b2a5bbfb2f
Madz96 <mad.subasinghe@gmail.com>
2019-06-20 10:15:54 +0530

    fifth

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -10,10 +10,11 @@ import { Todo } from "../models/Todo";
 export class TodoService {
   todosUrl: string =
     "https://my-json-server.typicode.com/Madz96/angular-crash-todolist/todos";
+  todosLimit = "?_limit=5";
 
   constructor(private http: HttpClient) {}
 
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todosUrl);
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
