a3988e2cf933ebe91d94322d0c9c17659ab1263a
Madz96 <mad.subasinghe@gmail.com>
2019-06-20 08:31:53 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name: string = "Madz";
+
+  constructor() {
+    this.changeName("Jon");
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+  }
 }
